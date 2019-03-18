3d71c8e9b5fad46275b01993fb19600e86035b12
voger <alktsam@yahoo.gr>
2019-03-18 23:25:07 +0200

    Finished transition to Promises

--- source/class/qxgraphql/HTTP.js
+++ source/class/qxgraphql/HTTP.js
@@ -1,35 +1,44 @@
 qx.Class.define("qxgraphql.HTTP", {
-  extend : qx.io.rest.Resource,
+  extend : qx.core.Object,
+
 
   /**
    * Tailor qx.io.rest.Resource to work as a graphql client 
    */
 
   construct : function(url) {
-    // only post is supported for now
-    var description = {
-      "post": { method: "POST", url: url}
-    };
+    this.base(arguments);
 
-    this.base(arguments, description);
+    if (url) { 
+      this.setUrl(url);
+    }
+  },
 
-    // set the configureRequest callback
-    this.configureRequest(this._getConfigureRequestCallback());
+  events: {
+  /**
+   * Fired when the request was successful
+   */
+    "success": "qxgraphql.event.type.GraphQL",
+
+  /**
+   * Fired when the request fails
+   */
+    "error": "qxgraphql.event.type.GraphQL"
+    
+
   },
 
   properties: {
     contentType: {
       check: "String",
       default: "application/json",
-      nullable: false,
-      event: "changeContentType"
+      nullable: false
     },
 
     accept: {
       check: "String",
       default: "application/json",
-      nullable: false,
-      event: "changeAccept"
+      nullable: false
     },
 
     /** The timeout for asynchronous calls in milliseconds.
@@ -40,59 +49,54 @@ qx.Class.define("qxgraphql.HTTP", {
       nullable: false,
       default: 0
 
+    },
+
+    url: {
+      validate: qx.util.Validate.url,
+      init: null
     }
   },
 
   members: {
-    _getConfigureRequestCallback() {
-      var resource = this;
-      var callback = function(req) {
-        req.setRequestHeader("Content-Type", resource.getContentType());
-        req.setAccept(resource.getAccept());
-      };
+    send: function(query, headers) {
+      return new qx.Promise(function(resolve, reject) {
+        var request = this.__getRequest();
+        request.setUrl(this.getUrl());
+        // only POST is supported
+        request.setMethod("POST");
+        request.setAccept(this.getAccept());
+        request.setTimeout(this.getTimeout());
+        request.setRequestHeader("Content-Type", this.getContentType());
 
-      return callback;
+
+        headers.keys.forEach(function(key) {
+          request.setRequestHeader(key, request[key]);
+        });
+
+        // add various listeners
+        request.addListenerOnce("success", function(request) {
+          var props = [request.getResponse(), null, false, request, request.getPhase()];
+          this.fireEvent("success", qxgraphql.event.type.GraphQL, props);
+          resolve(request);
+        }, this);
+
+        request.addListenerOnce("fail", function(request) {
+          var props = [request.getResponse(), null, false, request, request.getPhase()];
+          this.fireEvent("error", qxgraphql.event.type.GraphQL, props);
+          // console.log(request);
+          reject(new qx.type.BaseError("Failed"));
+        }, this);
+
+
+        request.addListenerOnce("loadEnd", function(request) {
+          request.dispose();
+        }, this);
+      });
     },
 
-    // Replace all qx.event.type.Rest with qxgraphql.event.type.GraphQL
-    // to make usage feel more consistent.
-    _tailorResource: function(resource) {
-      // inject different request implementation
-      resource.setRequestFactory(this._getRequest);
 
-      // inject different request handling
-      resource.setRequestHandler({
-        onsuccess: {
-          callback: function(req, action) {
-            return function() {
-              var props = [req.getResponse(), null, false, req, action, req.getPhase()];
-              this.fireEvent(action + "Success", qxgraphql.event.type.GraphQL, props);
-              this.fireEvent("success", qxgraphql.event.type.GraphQL, props);
-            };
-          },
-          context: this
-        },
-        onfail: {
-          callback: function(req, action) {
-            return function() {
-              var props = [req.getResponse(), null, false, req, action, req.getPhase()];
-              this.fireEvent(action + "Error", qxgraphql.event.type.GraphQL, props);
-              this.fireEvent("error", qxgraphql.event.type.GraphQL, props);
-            };
-          },
-          context: this
-        },
-        onloadend: {
-          callback: function(req, action) {
-            return function() {
-              req.dispose();
-            };
-          },
-          context: this
-        }
-      });
-
-      return resource;
+    __getRequest: function() {
+      return new qx.io.request.Xhr();
     },
 
 
@@ -104,5 +108,4 @@ qx.Class.define("qxgraphql.HTTP", {
       }
     }
   }
-
 });
--- source/class/qxgraphql/event/type/GraphQL.js
+++ source/class/qxgraphql/event/type/GraphQL.js
@@ -1,3 +1,42 @@
 qx.Class.define("qxgraphql.event.type.GraphQL", {
-  extend: qx.event.type.Rest
+  extend: qx.event.type.Data,
+
+  properties: {
+    /**
+     * The request of the event
+     */
+    request: {
+      check: "qx.io.request.AbstractRequest"
+    },
+
+    /**
+     * 
+     */
+    phase: {
+      check: "String"
+    }
+  },
+
+  members: {
+    /**
+     * Initializes an event object
+     */
+    init: function(data, old, cancelable, request, phase) {
+      this.base(arguments, data, old, cancelable);
+
+      this.setRequest(request);
+      this.setPhase(phase);
+
+      return this;
+    },
+
+    /**
+     * Get a copy of this object
+     */
+    clone: function(embryo) {
+      var clone = this.base(arguments, embryo);
+      clone.setPhase(this.getPhase());
+      clone.setRequest(this.getRequest());
+    }
+  }
 });
