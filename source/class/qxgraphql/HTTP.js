c27a4bffc95849e532e2a54cadff51d6c778cd6a
voger <alktsam@yahoo.gr>
2019-04-13 23:50:01 +0300

    Squashed commit of the following:
    commit b9265092e189352da69c86ccbb0f1ead26335a5b
    Author: voger <alktsam@yahoo.gr>
    Date:   Sat Apr 13 23:46:16 2019 +0300
    
        Add support for custom headers
    
    commit 9bd232b19adb02d80a7784299e409ff58db4306e
    Author: voger <alktsam@yahoo.gr>
    Date:   Sat Apr 13 23:43:23 2019 +0300
    
        Update compiler
    
    commit 781e8eac78a747c48de3f2731ae71ac5a0a05bad
    Author: voger <alktsam@yahoo.gr>
    Date:   Thu Apr 11 23:36:15 2019 +0300
    
        Add function to set a request header

--- source/class/qxgraphql/HTTP.js
+++ source/class/qxgraphql/HTTP.js
@@ -3,7 +3,7 @@ qx.Class.define("qxgraphql.HTTP", {
 
 
   /**
-   * Tailor qx.io.rest.Resource to work as a graphql client 
+   * Basic HTTP GraphQL client
    */
 
   construct : function(url) {
@@ -12,6 +12,12 @@ qx.Class.define("qxgraphql.HTTP", {
     if (url) { 
       this.setUrl(url);
     }
+
+    this.__requestHeaders = new Map();
+
+    // force _apply
+    this.initAccept();
+    this.initContentType();
   },
 
   events: {
@@ -32,12 +38,14 @@ qx.Class.define("qxgraphql.HTTP", {
     contentType: {
       check: "String",
       init: "application/json",
+      apply: "_applyContentType",
       nullable: false
     },
 
     accept: {
       check: "String",
       init: "application/json",
+      apply: "_applyAccept",
       nullable: false
     },
 
@@ -61,27 +69,97 @@ qx.Class.define("qxgraphql.HTTP", {
 
   members: {
 
+    __requestHeaders: null,
+
     /**
-     * Sends the query and returns a promise.
-     * @query 
+     * Returns a javascript Map() object with the headers that will be set
+     * to the request
      *
      */
-    send: function(query, headers, scope) {
-      var request = this.__getRequest();
-      request.setUrl(this.getUrl());
-      // only POST is supported
-      request.setMethod("POST");
-      request.setAccept(this.getAccept());
-      request.setTimeout(this.getTimeout());
-      request.setRequestHeader("Content-Type", this.getContentType());
-      request.setRequestData(query.toJson());
+    getRequestHeaders: function() {
+      return this.__requestHeaders;
+    },
 
-      // set the rest of the headers
-      // TODO: Add support for default headers object
-      headers = headers && headers.isObject() ? headers : {};
-      Object.keys(headers).forEach(function(key) {
-        request.setRequestHeader(key, request[key]);
-      });
+
+    /**
+     * Returns the value of the header or `undefined` if the 
+     * header is not set
+     *
+     */
+    getRequestHeader: function(key) {
+      this.getRequestHeaders().get(key);
+    },
+
+
+    /**
+     * Adds a request key to the requests. Case sensitive. Keys with different 
+     * casing will produce duplicate entries.
+     * @param key {String} The name of the header whose value is to be set.
+     * @param value {String}  The value to set as the body of the header.
+     */
+    setRequestHeader: function(key, value) {
+      let upperCase = key.toUpperCase();
+
+      // If the header is set from one of `this` properites
+      // treat it specially 
+      switch (upperCase) {
+        case "ACCEPT": 
+          this.setAccept(value);
+          break;
+        case "CONTENT-TYPE":
+          this.setContentType(value);
+          break;
+        default:
+          this.__requestHeaders.set(key, value);
+      }
+    },
+
+    /**
+     * Removes a request header
+     * @param key {String} The header to be removed
+     */
+    removeRequestHeader: function(key) {
+      let upperCase = key.toUpperCase();
+
+      // If the header is set from one of `this` properites
+      // treat it specially 
+      switch (upperCase) {
+        case "ACCEPT":
+          this.setAccept(null);
+          break;
+        case "CONTENT-TYPE":
+          this.setContentType(null);
+          break;
+        default:
+          this.getRequestHeaders().delete(key);
+      }
+    },
+
+
+    /**
+     * Sends the query and returns a promise.
+     * @query {String} The query to send
+     * @headers {Object} An object of key value pairs with the request headers. 
+     *                   These headers will be used instead of the default headers 
+     *                   set in this object
+     * @scope {Object} The scope to which the promise object is bound
+     * @return {qx.Promise}
+     */
+    send: function(query, headers, scope) {
+      const request = this.__getRequest();
+      request.setUrl(this.getUrl());
+      // only POST is supported for now
+      request.setMethod("POST");
+      request.setTimeout(this.getTimeout());
+
+      const requestHeaders = headers ? 
+        Object.entries(headers) : this.getRequestHeaders();
+
+      requestHeaders.forEach((value, header) => 
+        request.setRequestHeader(header, value));
+
+
+      request.setRequestData(query.toJson());
 
       // cache the scope 
       var service = this;
@@ -117,11 +195,28 @@ qx.Class.define("qxgraphql.HTTP", {
     },
 
 
+
     _validateTimeout: function(value) {
       try {
         qx.core.Assert.assertPositiveInteger(value);
       } catch (e) {
-        throw new qx.core.ValidationError("ValidationError: " + value + " must be a positive integer.");
+        throw new qx.core.ValidationError(`ValidationError: Time out value must be a positive integer. Found : ${value}.`);
+      }
+    },
+
+    _applyAccept: function(value) {
+      if (value === null) {
+        this.removeRequestHeader("Accept");
+      } else {
+        this.getRequestHeaders().set("Accept", value);
+      }
+    },
+
+    _applyContentType: function(value) {
+      if (value === null) {
+        this.removeRequestHeader("Content-Type");
+      } else {
+        this.getRequestHeaders().set("Content-Type", value);
       }
     }
   }
