f641005c1219b47cb05471ff79dd406091e4665e
voger <voger@runbox.com>
2019-11-03 19:39:55 +0200

    Removed testapper

--- qx_packages/qooxdoo_qxl_testtapper_v0_4_0/source/class/qxl/testtapperdemo/test/Test05BadTest.js
+++
@@ -1,9 +0,0 @@
-qx.Class.define("qxl.testtapperdemo.test.Test05BadTest", {
-  extend: qx.dev.unit.TestCase,
-  members: {
-    "test01: Unknown Function": function () {
-        this.CallUnknownFunctionInMethod();
-    }
-  }
-
-});
--- qx_packages/qooxdoo_qxl_testtapper_v0_4_0/source/class/qxl/testtapperdemo/test/Test08Exception.js
+++
@@ -1,8 +0,0 @@
-qx.Class.define("qxl.testtapperdemo.test.Test08Exception", {
-  extend: qx.dev.unit.TestCase,
-  members: {
-    "test01: die baby die": function () {
-        this.callUnknownFunction();
-    },
-  }
-});
